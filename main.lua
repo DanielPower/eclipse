5184bfbd0a1707848b94c01134c70f9b6e5e1446
Daniel Power <me@danielpower.ca>
2023-06-13 23:19:56 -0230

    Shader navigation, error handling

--- main.lua
+++ main.lua
@@ -1,24 +1,64 @@
 local util = require("util")
 
 local time
-local shader
 local files
+local toyIndex = 1
+local toy
+local errorMessage
+
+local function loadToy()
+	local status, result = pcall(function()
+		toy = util.newToy("shaders/" .. files[toyIndex], love.graphics.getWidth(), love.graphics.getHeight(), time)
+	end)
+	if status then
+		errorMessage = nil
+	elseif not errorMessage then
+		print(result)
+		errorMessage = result
+	end
+end
 
 function love.load()
 	time = 0
-	shader = util.parseShader("introduction")
+	files = love.filesystem.getDirectoryItems("shaders")
+	loadToy()
 end
 
 function love.update(dt)
 	time = time + dt
-	shader:send("iTime", time)
+	toy.update(time)
+	local modified = love.filesystem.getInfo("shaders/" .. files[toyIndex]).modtime
+	if modified ~= toy.modified then
+		loadToy()
+	end
 end
 
 function love.draw()
-	love.graphics.setShader(shader)
-	love.graphics.rectangle("fill", 0, 0, love.graphics.getWidth(), love.graphics.getHeight())
+	toy.draw()
+	if errorMessage then
+		love.graphics.setColor(0, 0, 0, 0.7)
+		love.graphics.rectangle("fill", 0, 0, love.graphics.getWidth(), love.graphics.getHeight())
+		love.graphics.setColor(1, 1, 1, 1)
+		love.graphics.setNewFont(16)
+		love.graphics.printf(errorMessage, 10, 10, love.graphics.getWidth() - 20)
+	end
 end
 
 function love.resize(w, h)
-	shader:send("iResolution", { love.graphics.getWidth(), love.graphics.getHeight() })
+	toy.resize(w, h)
+end
+
+function love.keypressed(key)
+	if key == "j" then
+		toyIndex = (toyIndex % #files) + 1
+		loadToy()
+	end
+	if key == "k" then
+		toyIndex = (toyIndex - 2) % #files + 1
+		loadToy()
+	end
+	if key == "r" then
+		time = 0
+		loadToy()
+	end
 end
--- util.lua
+++ util.lua
@@ -1,20 +1,42 @@
 local util = {}
 
-function util.parseShader(name)
-	local shaderString = [[
+function util.newToy(file, initialWidth, initialHeight, initialTime)
+	local shader = love.graphics.newShader([[
 #pragma language glsl3
 uniform float iTime;
 uniform vec2 iResolution;
-]] .. love.filesystem.read("shaders/" .. name .. ".glsl") .. [[
+]] .. love.filesystem.read(file) .. [[
 vec4 effect(vec4 fragColor, Image tex, vec2 textureCoords, vec2 screenCoords) {
   mainImage(fragColor, screenCoords);
   return fragColor;
 }
-]]
-	local shader = love.graphics.newShader(shaderString)
-	shader:send("iTime", 0)
-	shader:send("iResolution", { love.graphics.getWidth(), love.graphics.getHeight() })
-	return shader
+]])
+	local toy = {}
+	toy.modified = love.filesystem.getInfo(file).modtime
+	local time = initialTime
+	local width = initialWidth
+	local height = initialHeight
+	shader:send("iTime", time)
+	shader:send("iResolution", { width, height })
+
+	function toy.update(newTime)
+		time = newTime
+		shader:send("iTime", time)
+	end
+
+	function toy.resize(w, h)
+		width = w
+		height = h
+		shader:send("iResolution", { width, height })
+	end
+
+	function toy.draw()
+		love.graphics.setShader(shader)
+		love.graphics.rectangle("fill", 0, 0, width, height)
+		love.graphics.setShader()
+	end
+
+	return toy
 end
 
 return util
